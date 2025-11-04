6bd788fba5a2e8698ae71ad4f336fd54aaa82907
ammarr-arch <newamar5@gmail.com>
2025-11-04 23:18:10 +0700

    Delete Project Embedded System directory

--- Project Embedded System/src/modules/relay/module_relay.h
+++
@@ -1,8 +0,0 @@
-#ifndef MODULE_RELAY_H
-#define MODULE_RELAY_H
-
-void initRelay(int pin);
-void relayOn();
-void relayOff();
-
-#endif
--- Project Embedded System/src/modules/sensor_PZEM/module_sensor_PZEM.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "config/config_requirement.h"
-
-void setupSensor();
-void readSensor(float &v, float &i, float &p, float &e, float &pf);
